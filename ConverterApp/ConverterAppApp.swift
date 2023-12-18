f78bb25781071e0627e06602cd92e270e98b12b9
rockgard3n <liamcashel@gmail.com>
2023-12-18 16:40:50 -0500

    Initial Commit

---
+++ ConverterApp/ConverterAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConverterAppApp.swift
+//  ConverterApp
+//
+//  Created by Liam Cashel on 12/18/23.
+//
+
+import SwiftUI
+
+@main
+struct ConverterAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
